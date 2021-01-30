f047d352a45810a8a7699a0911a8325801d059c1
sol <g-h-0-s-t@protonmail.com>
2021-01-30 07:18:57 +0000

    chore: fixed new look

--- packages/client/src/components/Card/index.tsx
+++ packages/client/src/components/Card/index.tsx
@@ -3,10 +3,9 @@ import { SectionDataFragment } from "../../graphql/generated";
 import { AirbnbCarousel } from "../AirbnbCarousel";
 import X from "../../assets/images/xxx.png";
 import XX from "../../assets/images/wallpaper.png";
-import e from "../../assets/images/e.png";
-import ee from "../../assets/images/stars.png";
+import e from "../../assets/images/review.png";
+import StarsImage from "../../assets/images/stars.png";
 import { BsArrowRight } from "react-icons/bs";
-import ReviewCard from "../generated/ReviewCard/index";
 
 interface CardProps extends SectionDataFragment {}
 
@@ -18,23 +17,39 @@ export const Card: React.FC<CardProps> = (props: CardProps) => {
   const textColor = props.light ? "text-white" : "text-gray-700";
 
   return (
-    <div className="flex justify-center w-full px-12 bg-blue-700">
+    <div className="flex justify-center w-full px-4 pb-24 bg-blue-700 md:px-12">
       <div className="max-w-5xl">
-        <img className="w-12 mt-12" alt="wallpaper" src={XX} />
+        <div className="items-end justify-between md:flex">
+          <img className="w-12 mt-12" alt="wallpaper" src={XX} />
+          <p className="hidden font-bold md:block">
+            £<span className="text-6xl">500</span> PER ROOM
+          </p>
+        </div>
 
         <h2 className="mt-12 text-2xl text-white">Wallpapering Services</h2>
-        <div className="flex items-center w-auto p-12 mt-6 -mx-12 space-x-12 bg-white">
-          <p className="max-w-md text-xl leading-relaxed text-gray-700">
+        <div className="flex items-center w-auto p-4 mt-6 -mx-4 space-x-12 bg-white md:-mx-12 md:p-12">
+          <p className="max-w-md text-sm leading-relaxed text-gray-700 md:text-xl">
             Years of Experience in Exterior/Interior Painting and decorating of
             private homes, communal areas and commercial spaces.
           </p>
-          <BsArrowRight className="text-4xl" />
+          <BsArrowRight className="w-16 text-4xl" />
         </div>
-        <div className="relative flex justify-center w-full ">
-          <img className="mt-12 shadow-lg" alt="" src={X} />
-          <div className="absolute bottom-0 left-0 flex flex-col items-center bg-gray-900">
-            <ReviewCard />
+        <div className="relative flex justify-center w-auto -mx-4 md:mx-0 md:w-full">
+          <img className="mt-12 shadow-2xl" alt="" src={X} />
+
+          <div className="absolute bottom-0 left-0 hidden w-5/12 -mb-12 -ml-12 md:block">
+            <img alt="review card" src={e} />
           </div>
+        </div>
+
+        <div className="flex items-end justify-between mt-6 md:hidden">
+          <div>
+            <h6>reviews</h6>
+            <img src={StarsImage} alt="review stars" />
+          </div>
+          <p className="font-bold">
+            £<span className="text-4xl">500</span> PER ROOM
+          </p>
         </div>
       </div>
     </div>
